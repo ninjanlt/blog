ffa88124c05ee4b01a5879adb155f36c9fe26a63
NaLuTuo <57385332+lxmob@users.noreply.github.com>
2021-12-08 15:02:55 +0800

    继承

--- js/extend.js
+++ js/extend.js
@@ -1,35 +1,137 @@
 /**
- * 继承
- * 1. 原型链继承
- *     通过将子类的原型对象作为父类的实例
- *     缺点就是不能向父类构造函数进行传参、父类身上的引用类型会被实例共享
- * 2. 盗用构造函数继承
- *     通过在子类中使用 call 方法，实现盗用父类构造函数向父类传参
- *     缺点无法继承父类原型对象的属性和方法
- * 3. 组合继承
- *     将以上两种方法进行合并
- *     缺点父类构造函数会被调用多次
- * 4. 寄生组合继承
- *     建立在组合继承基础之上，使用 Object.create 方法
- *     缺点存在效率问题
+ * 原型链继承：
+ *  通过将子类的原型对象作为父类的实例
+ * 缺点：
+ *  引用类型的属性被所有实例共享
+ *  在创建子类的实例时，不能向父类传参
  * 
  */
 
-function Animal(name) {
+function Parent() {
+    this.name = 'kevin';
+}
+
+Parent.prototype.getName = function () {
+    console.log(this.name);
+}
+
+function Child() { }
+
+Child.prototype = new Parent();
+Child.prototype.constructor = Child;
+
+var child1 = new Child();
+console.log(child1.getName())
+
+
+/**
+ * 借用构造函数继承：
+ *  通过 call 去改变 this 指向执行父类方法
+ * 缺点：
+ *  子类不能访问父类原型身上定义的方法
+ * 
+ */
+function Parent(name) {
     this.name = name;
-    this.colors = ['red', 'blue'];
 }
-Animal.prototype.eat = function () {
-    console.log(this.name + ' is eatting');
+
+function Child(name) {
+    Parent.call(this, name);
 }
-function Dog(name) {
-    Animal.call(this, name);
+
+var child1 = new Child('kevin');
+console.log(child1.name); // kevin
+
+var child2 = new Child('daisy');
+console.log(child2.name); // daisy
+
+
+/**
+ * 组合继承
+ *  结合原型链继承和构造函数继承方式
+ * 缺点：
+ *  父类构造函数始终会被调用两次
+ * 
+ */
+function Parent(name) {
+    console.log('执行Parent')
+    this.name = name;
+    this.colors = ['red', 'blue', 'green'];
+    this.publicMethod = () => {
+    }
 }
-Dog.prototype = Object.create(Animal.prototype);
-Dog.prototype.constructor = Dog;
-var dog1 = new Dog('dog1');
-var dog2 = new Dog('dog2');
-dog1.colors.push('yellow');
-console.log(dog1.name);  // 输出dog1
-console.log(dog2.colors);// 输出['red','blue']
-console.log(dog2.eat()); // 输出dog2 is eatting
+
+Parent.prototype.getName = function () {
+    console.log(this.name)
+}
+
+function Child(name, age) {
+    Parent.call(this, name);
+    this.age = age;
+}
+
+Child.prototype = new Parent();
+Child.prototype.constructor = Child;
+
+var child1 = new Child('kevin', '18');
+
+child1.colors.push('black');
+
+console.log(child1.name); // kevin
+console.log(child1.age); // 18
+console.log(child1.colors); // ["red", "blue", "green", "black"]
+
+var child2 = new Child('daisy', '20');
+
+console.log(child2.name); // daisy
+console.log(child2.age); // 20
+console.log(child2.colors); // ["red", "blue", "green"]
+
+
+/**
+ * 寄生式继承
+ *  构造函数和工厂的模式，创建一个实现继承的函数，以某种方式增强对象
+ * 
+ */
+function createObj(o) {
+    var clone = Object.create(o);
+    clone.sayName = function () {
+        console.log('hi');
+    }
+    return clone;
+}
+
+
+/**
+ * 寄生组合式继承
+ *  不通过调用父类构造函数给子类原型赋值，而是取得父类原型的一个副本
+ * 
+ */
+function inheritPrototype(subType, superType) {
+    let prototype = Object.create(superType.prototype); // 创建对象
+    prototype.constructor = subType; // 增强对象
+    subType.prototype = prototype; // 赋值对象
+}
+
+function SuperType(name) {
+    this.name = name;
+    this.colors = ["red", "blue", "green"];
+}
+
+SuperType.prototype.sayName = function () {
+    console.log(this.name);
+};
+
+function SubType(name, age) {
+    console.log('执行SubType')
+    SuperType.call(this, name);
+    this.age = age;
+}
+
+inheritPrototype(SubType, SuperType);
+SubType.prototype.sayAge = function () {
+    console.log(this.age);
+};
+
+let subTupe1 = new SubType('name1', 1)
+let subTupe2 = new SubType('name2', 2)
